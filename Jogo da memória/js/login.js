0187b20fec2ece97e6c611c35cee9c0a3ec1ce89
Lavínia <laviniaof@hotmail.com>
2022-10-24 22:13:50 -0300

    ATUA: correção de erros; programa funcionando 100%

--- Jogo da memória/js/login.js
+++ Jogo da memória/js/login.js
@@ -1,9 +1,9 @@
-const input = document.querySelector('.login__input');
-const button = document.querySelector('.login__button');
-const form = document.querySelector('.login-form');
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login_form');
 
 const validateInput = ({ target }) => {
-  if (target.value.length > 2) {
+  if (target.value.length > 3) {
     button.removeAttribute('disabled');
     return;
   }
