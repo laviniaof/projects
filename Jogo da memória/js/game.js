c931c48856221a6231ad95723f37258495a2e22c
Lavínia <laviniaof@hotmail.com>
2022-10-25 14:47:39 -0300

    ATUALIZAÇÃO III

--- Jogo da memória/js/game.js
+++ Jogo da memória/js/game.js
@@ -19,25 +19,80 @@ const createElement = (tag, className) => {
     return element;
 }
 
+let firstCard = '';
+let secondCard = '';
+
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if (firstCharacter === secondCharacter) {
+
+
+
+    }else {
+      setTimeout(() => {
+
+      firstCard.classList.remove('reveal-card');
+      secondCard.classList.remove('reveal-card');
+
+      firstCard = '';
+      secondCard = '';
+
+      }, 500);
+
+    }
+}
+
+const revealCard = ({target}) => {
+
+    if(target.parentNode.className.includes('reveal-card')) {
+        return;
+    }
+
+    if (firstCard === '') {
+
+        target.parentNode.className.includes('reveal-card');
+        firstCard = target.parentNode;
+
+    } else if (secondCard === '') {
+       
+        target.parentNode.className.includes('reveal-card');
+        secondCard = target.parentNode;
+
+    }
+
+
+
+    target.parentNode.classList.add('reveal-card');
+
+}
+
 const createCard = (character) => {
 
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../images/${character}.png')`;
+    front.style.backgroundImage = `url('../img/${character}.jpeg')`;
 
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-character', character);
     return card; 
 }
 
 const loadGame = () => {
 
     const duplicateCharacters = [ ...characters, ...characters ];
-    duplicateCharacters.forEach((character) => {
-         
+
+    const shuffledArray = duplicateCharacters.sort( () => Math.random() - 0.5 );
+
+    shuffledArray.forEach((character) => {
+
         const card = createCard(character);
         grid.appendChild(card);
 
